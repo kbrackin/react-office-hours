c1d786ec6bff6da5a4b4348f0dab5d379b9257fb
kbrackin <135669996+kbrackin@users.noreply.github.com>
2023-11-21 18:32:40 -0500

    tech profile with express server

--- server.js
+++ server.js
@@ -17,6 +17,16 @@ app.get('/api/profile', (req,res) => {
 })
 
 //HTML View
-app.get("/", (req,res) => res.send('HTML is under construction'))
+// app.get("/", (req,res) => res.send('HTML is under construction'))
+
+// app.get('/css/app.css', (req,res) => {
+//     res.sendFile(__dirname + `/public/css/app.css`)
+// })
+
+// app.get('/images/vite.svg', (req,res) => {
+//     res.sendFile(__dirname + `/public/css/app.css`)
+// })
+
+app.use(express.static("public"))
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
